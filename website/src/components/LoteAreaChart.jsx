6dc7e0dc92143acd4a225ba17df5098d2e910746
Marcos Capistrano <mcapistrano50@gmail.com>
2022-09-28 13:55:25 -0300

    estourooo web funfando

---
+++ website/src/components/LoteAreaChart.jsx
@@ -0,0 +1,146 @@
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Filler,
+    Legend,
+} from 'chart.js';
+import { Line, getDatasetAtEvent } from 'react-chartjs-2';
+import { useState, useEffect, useRef } from 'react';
+
+
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Filler,
+    Legend
+);
+
+
+
+function extrapolateData(dataArray, labels) {
+    let newDataArray = [];
+    let lastValue = 0;
+    for (let label of labels) {
+        for (let entry of dataArray) {
+            if (entry.date == label) {
+                lastValue = entry.value;
+                break;
+            }
+        }
+
+        newDataArray.push(lastValue);
+    }
+
+    return newDataArray;
+}
+
+function createDataset(sublote, chart) {
+    return {
+        labels: sublote.labels.map(entry => new Date(entry * 1000).toLocaleString('pt-BR')),
+        datasets: [
+            {
+                label: 'Entrada',
+                data: extrapolateData(sublote.sensor_entr, sublote.labels),
+                borderColor: 'rgba(0, 0, 128, 0.5)',
+                backgroundColor: 'rgba(0, 0, 128, 0.1)',
+            },
+            {
+                label: 'Massa 1',
+                data: extrapolateData(sublote.sensor_m1, sublote.labels),
+                borderColor: 'rgba(0, 128, 128, 0.5)',
+                backgroundColor: 'rgba(0, 128, 128, 0.1)',
+            },
+            {
+                label: 'Massa 2',
+                data: extrapolateData(sublote.sensor_m2, sublote.labels),
+                borderColor: 'rgba(170, 110, 40, 0.5)',
+                backgroundColor: 'rgba(170, 110, 40, 1)',
+            },
+            {
+                label: 'Massa 3',
+                data: extrapolateData(sublote.sensor_m3, sublote.labels),
+                borderColor: 'rgba(245, 130, 48, 0.5)',
+                backgroundColor: 'rgba(245, 130, 48, 1)',
+            },
+            {
+                label: 'Massa 4',
+                data: extrapolateData(sublote.sensor_m4, sublote.labels),
+                borderColor: 'rgba(220, 190, 255, 0.5)',
+                backgroundColor: 'rgba(220, 190, 255, 1)',
+            },
+        ]
+    }
+}
+
+export default function LoteAreaChart({ sublote }) {
+    const [data, setData] = useState({ datasets: [] });
+    const [options, setOptions] = useState({
+        responsive: true,
+        plugins: {
+            legend: {
+                position: 'top',
+            },
+            title: {
+                display: false,
+                text: "",
+            },
+        },
+    });
+    const chartRef = useRef(null);
+
+    useEffect(() => {
+        const chart = chartRef.current;
+
+        if (!chart)
+            return;
+
+        console.log(createDataset(sublote, chart));
+        // setOptions({...options, plugins: { ...options.plugins, title: { display: true, text: "oi"}}});
+        setData(createDataset(sublote, chart));
+    }, []);
+
+    return (
+        <div className='w-2/3 h-full mx-auto py-12 text-center'>
+            <h1 className='text-2xl font-bold text-gray-800'>Ligado em: {new Date(sublote.initDate * 1000).toLocaleString('pt-BR')}</h1>
+            {sublote.endDate ? <h1 className='text-lg text-gray-700 pb-8'>Desligado em: {new Date(sublote.endDate * 1000).toLocaleString('pt-BR')}</h1> : null}
+            <Line ref={chartRef} options={options} data={data} />
+        </div>
+    );
+}
+
+function createGradientStroke(ctx, area) {
+    const colorStart = "rgba(0, 0, 255, 0.5)";
+    const colorMid = "rgba(0, 255, 0, 0.5)";
+    const colorEnd = "rgba(255, 0, 0, 0.5)";
+
+    const gradient = ctx.createLinearGradient(0, area.bottom, 0, area.top);
+
+    gradient.addColorStop(0, colorStart);
+    gradient.addColorStop(0.5, colorMid);
+    gradient.addColorStop(1, colorEnd);
+
+    return gradient;
+}
+
+function createGradientBackground(ctx, area) {
+    const colorStart = "rgba(0, 0, 255, 0.2)";
+    const colorMid = "rgba(0, 255, 0, 0.2)";
+    const colorEnd = "rgba(255, 0, 0, 0.2)";
+
+    const gradient = ctx.createLinearGradient(0, area.bottom, 0, area.top);
+
+    gradient.addColorStop(0, colorStart);
+    gradient.addColorStop(0.5, colorMid);
+    gradient.addColorStop(1, colorEnd);
+
+    return gradient;
+}
--- website/src/pages/LoteDetails.jsx
+++ website/src/pages/LoteDetails.jsx
@@ -1,105 +1,10 @@
 import React, { useState } from 'react';
 import csv from 'csvtojson';
 import Axios from "axios";
-import { Line } from 'react-chartjs-2';
-import {
-    Chart as ChartJS,
-    CategoryScale,
-    LinearScale,
-    PointElement,
-    LineElement,
-    Title,
-    Tooltip,
-    Legend,
-} from 'chart.js';
 import { useEffect } from 'react';
 import axios from 'axios';
+import LoteAreaChart from '../components/LoteAreaChart';
 
-ChartJS.register(
-    CategoryScale,
-    LinearScale,
-    PointElement,
-    LineElement,
-    Title,
-    Tooltip,
-    Legend
-);
-
-export const options = {
-    responsive: true,
-    plugins: {
-        legend: {
-            position: 'top',
-        },
-        title: {
-            display: true,
-            text: 'Chart.js Line Chart',
-        },
-    },
-};
-
-function parseHistorico(json) {
-    let result = {
-        initDate: null,
-        endDate: null,
-
-        sublotes: []
-    }
-
-    let subloteCount = 0;
-    for (let entry of json) {
-        if (entry.sensor === "LOTE") {
-            if (entry.value === "1") {
-                result.initDate = new Date(Number(entry.date) * 1000);
-            } else if (entry.value === "0") {
-                result.endDate = new Date(Number(entry.date) * 1000);
-            }
-        } else if (entry.sensor === "DEVICE_STATE") {
-            if (entry.value === "1") {
-                result.sublotes.push({
-                    initDate: new Date(Number(entry.date) * 1000),
-                    endDate: null,
-
-                    sensor_entr: [],
-                    sensor_m1: [],
-                    sensor_m2: [],
-                    sensor_m3: [],
-                    sensor_m4: [],
-                })
-            } else if (entry.value === "0") {
-                result.sublotes[subloteCount].endDate = new Date(Number(entry.date) * 1000);
-                subloteCount++;
-            }
-        } else if (entry.sensor === "SENSOR_ENTR") {
-            result.sublotes[subloteCount].sensor_entr.push({
-                date: new Date(Number(entry.date) * 1000),
-                value: entry.value
-            })
-        } else if (entry.sensor === "SENSOR_M1") {
-            result.sublotes[subloteCount].sensor_m1.push({
-                date: new Date(Number(entry.date) * 1000),
-                value: entry.value
-            })
-        } else if (entry.sensor === "SENSOR_M2") {
-            result.sublotes[subloteCount].sensor_m2.push({
-                date: new Date(Number(entry.date) * 1000),
-                value: entry.value
-            })
-        } else if (entry.sensor === "SENSOR_M3") {
-            result.sublotes[subloteCount].sensor_m3.push({
-                date: new Date(Number(entry.date) * 1000),
-                value: entry.value
-            })
-        } else if (entry.sensor === "SENSOR_M4") {
-            result.sublotes[subloteCount].sensor_m4.push({
-                date: new Date(Number(entry.date) * 1000),
-                value: entry.value
-            })
-        }
-    }
-
-    return result;
-}
 
 let rawData = `1664313661,LOTE,1
 1664313661,DEVICE_STATE,1
@@ -108,14 +13,51 @@ let rawData = `1664313661,LOTE,1
 1664313661,SENSOR_M2,0
 1664313661,SENSOR_M3,0
 1664313661,SENSOR_M4,0
-1664313662,SENSOR_ENTR,22`;
-
-const labels = ['January', 'February', "march"];
+1664313661,SENSOR_ENTR, 22
+1664313661,SENSOR_M1,23
+1664313661,SENSOR_M2,32
+1664313661,SENSOR_M3,25
+1664313661,SENSOR_M4,21
+1664313671,SENSOR_ENTR, 24
+1664313681,SENSOR_M1,23
+1664313691,SENSOR_M2,0
+1664313701,SENSOR_M3,25
+1664313711,SENSOR_M4,12
+1664313721,SENSOR_ENTR, 22
+1664313731,SENSOR_M1,23
+1664313751,SENSOR_M2,0
+1664318061,SENSOR_M3,25
+1664318361,SENSOR_M4,0
+1664318361,SENSOR_ENTR, 22
+1664318361,SENSOR_M1,23
+1664319261,SENSOR_M2,21
+1664319761,SENSOR_M3,25
+1664320661,SENSOR_M4,21
+1664333662,SENSOR_ENTR,22
+1664343661,DEVICE_STATE,0
+1664353661,DEVICE_STATE,1
+1664363681,SENSOR_M1,23
+1664373691,SENSOR_M2,0
+1664383701,SENSOR_M3,25
+1664393711,SENSOR_M4,12
+1664403721,SENSOR_ENTR, 22
+1664413731,SENSOR_M1,23
+1664423751,SENSOR_M2,0
+1664438061,SENSOR_M3,25
+1664448361,SENSOR_M4,0
+1664458361,SENSOR_ENTR, 22
+1664468361,SENSOR_M1,23
+1664479261,SENSOR_M2,21
+1664489761,SENSOR_M3,25
+1664490661,SENSOR_M4,21
+1664503662,SENSOR_ENTR,22
+`;
 
 export default function LoteDetails() {
+    const [loading, setLoading] = useState(true);
     const [initDate, setInitDate] = useState(null);
     const [endDate, setEndDate] = useState(null);
-    const [data, setData] = useState(null);
+    const [sublotes, setSublotes] = useState([]);
 
     useEffect(() => {
         async function fetchData() {
@@ -126,53 +68,117 @@ export default function LoteDetails() {
 
                 setInitDate(result.initDate);
                 setEndDate(result.endDate);
-
-                console.log();
-
-                setData({
-                    labels,
-                    datasets: [
-                        {
-                            label: 'Dataset 1',
-                            data: result.sublotes[0].sensor_entr.map(data => Number(data.value)),
-                            borderColor: 'rgb(255, 99, 132)',
-                            backgroundColor: 'rgba(255, 99, 132, 0.5)',
-                        },
-                    ],
-                })
-            })
+                setSublotes(result.sublotes);
+                setLoading(false);
+            });
         }
+
         fetchData();
-
-        // try {
-        // const response = await axios.get('/lote/1');
-        // rawData = response.data.replace("\n", ",");
-        // rawData = rawData.replace("\n", ",");
-        // console.log(rawData);
-        // } catch (error) {
-        //     console.error(error);
-        // }
-
-
-
-
-        // }, [someId]); // Or [] if effect doesn't need props or state
     }, []);
 
-    if (!data) {
+    if (loading) {
         return <pre>Loading...</pre>
-    } else {
-        console.log(data)
     }
 
     return (
         <>
             <h1 className="text-center text-3xl pb-8">Lote 1</h1>
-            <Line options={options} data={data} />;
-            {/* <Line
-                options={options}
-                data={data}
-            /> */}
+            {sublotes.map(sublote => (
+                <div className='items-center'>
+                    <LoteAreaChart sublote={sublote} />
+                </div>
+            ))}
         </>
     )
+}
+
+function parseHistorico(json) {
+    let result = {
+        initDate: null,
+        endDate: null,
+
+        sublotes: []
+    }
+
+    let validLote = false;
+    let subloteCount = 0;
+    for (let i = 0; i < json.length; i++) {
+        let entry = json[i];
+        let date = Number(entry.date);
+        let value = entry.value;
+
+        if (entry.sensor === "LOTE") {
+            if (value === "1") {
+                result.initDate = date;
+                validLote = true;
+            } else if (entry.value === "0") {
+                result.endDate = date;
+                validLote = false;
+            }
+        }
+
+        if (validLote) {
+            if (entry.sensor === "DEVICE_STATE") {
+                if (value === "1") {
+                    result.sublotes.push({
+                        initDate: date,
+                        endDate: null,
+
+                        labels: [],
+                        sensor_entr: [],
+                        sensor_m1: [],
+                        sensor_m2: [],
+                        sensor_m3: [],
+                        sensor_m4: [],
+                    })
+                } else if (value === "0") {
+                    result.sublotes[subloteCount].endDate = date;
+                    subloteCount++;
+                }
+
+            } else if (entry.sensor === "SENSOR_ENTR") {
+                result.sublotes[subloteCount].sensor_entr.push({
+                    date: date,
+                    value: value
+                })
+
+                if (result.sublotes[subloteCount].labels.includes(date) === false)
+                    result.sublotes[subloteCount].labels.push(date);
+            } else if (entry.sensor === "SENSOR_M1") {
+                result.sublotes[subloteCount].sensor_m1.push({
+                    date: date,
+                    value: value
+                })
+
+                if (result.sublotes[subloteCount].labels.includes(date) === false)
+                    result.sublotes[subloteCount].labels.push(date);
+            } else if (entry.sensor === "SENSOR_M2") {
+                result.sublotes[subloteCount].sensor_m2.push({
+                    date: date,
+                    value: value
+                })
+
+                if (result.sublotes[subloteCount].labels.includes(date) === false)
+                    result.sublotes[subloteCount].labels.push(date);
+            } else if (entry.sensor === "SENSOR_M3") {
+                result.sublotes[subloteCount].sensor_m3.push({
+                    date: date,
+                    value: value
+                })
+
+                if (result.sublotes[subloteCount].labels.includes(date) === false)
+                    result.sublotes[subloteCount].labels.push(date);
+            } else if (entry.sensor === "SENSOR_M4") {
+                result.sublotes[subloteCount].sensor_m4.push({
+                    date: date,
+                    value: value
+                })
+
+                if (result.sublotes[subloteCount].labels.includes(date) === false)
+                    result.sublotes[subloteCount].labels.push(date);
+            }
+        }
+    }
+
+    return result;
 }
